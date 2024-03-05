9368cce19eabd65651e988e3baf3cf33d704cf2d
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-03-05 09:53:21 +0000

    e

--- TC-mingw.cmake
+++
@@ -1,8 +0,0 @@
-# TC-mingw.cmake
-set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-set(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-set(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc /home/alex/mingw-install)
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
